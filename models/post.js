cebf49b3bb52fd2d8b46f567f52f4cf1656b468f
syrinehamdoun <hamdoun.syrine@gmail.com>
2022-01-04 00:24:39 +0100

    posts crud calender

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose =require('mongoose')
+const Schema=mongoose.Schema
+
+
+const PostShema= new Schema({
+	_id: Schema.Types.ObjectId,
+	description: {
+		type: String,
+	},	
+	date_creation: {
+		type:Date,
+		default:Date.now
+	},
+    deleted: {
+		type: Boolean,
+		default:0
+	}
+
+
+})
+module.exports=Item=mongoose.model('post',PostShema)
